06e24d3b775d0c679ddb017cab195d23f26710c2
xoxokss <sangseon14@gmail.com>
2022-09-24 16:42:00 +0900

    FIX

--- dist/app.js
+++ dist/app.js
@@ -3,12 +3,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const index_1 = require("./routes/index");
-const app = (0, express_1.default)();
+var express_1 = __importDefault(require("express"));
+var index_1 = require("./routes/index");
+var app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: false }));
 app.use("/api", index_1.router);
-app.listen(8000, () => {
+app.listen(8000, function () {
     console.log("server start");
 });
--- dist/routes/index.js
+++ dist/routes/index.js
@@ -4,8 +4,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.router = void 0;
-const express_1 = __importDefault(require("express"));
-const router = express_1.default.Router();
+var express_1 = __importDefault(require("express"));
+var router = express_1.default.Router();
 exports.router = router;
 router.get('/', function (req, res) {
     res.send('api페이지');
