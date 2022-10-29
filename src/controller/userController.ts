7f9b9f322ee36f3142f06fd2680f25dfe91410d5
xoxokss <sangseon14@gmail.com>
2022-10-29 14:34:26 +0900

    Feat : ID,닉네임 중복검사 추가

--- src/controller/userController.ts
+++ src/controller/userController.ts
@@ -7,80 +7,140 @@ import bcrypt from "bcrypt";
 const saltRounds = 10;
 
 const userController = {
-	//회원가입
-	signup: async (req: Request, res: Response) => {
-		const { userId, email, password, nickname } = req.body;
-		try {
-			//비밀번호 암호화
-			const encryptPassword = bcrypt.hashSync(password, saltRounds);
-			const data = {
-				username: userId,
-				email: email,
-				nickname: nickname,
-				password: encryptPassword,
-			};
-			//DB에 유저정보 create
-			await prisma.user.create({
-				data: data,
-			});
-			//response
-			res.send({
-				data: "success",
-			});
-		} catch (error) {
-			console.log(error);
-		} finally {
-			async () => {
-				await prisma.$disconnect();
-			};
-		}
-	},
-	//로그인
-	login: async (req: Request, res: Response) => {
-		const { userId, password } = req.body;
-		try {
-			//DB에서 유저 정보 찾기
-			const user = await prisma.user.findUnique({
-				where: {
-					username: userId,
-				},
-			});
-			//비밀번호 복호화 매치
-			const match = bcrypt.compareSync(password, user?.password ?? "");
-			if (!match) {
-				return res.status(400).send({ error: "로그인 정보를 확인하세요" });
-			}
-			//토큰 발행
-			const token = jwt.sign({ userId: user?.username }, "sangseon", {
-				expiresIn: "24h",
-			});
-			//response
-			res.status(200).send({ message: "로그인 성공", token });
-		} catch (error) {
-			console.log(error);
-			res.status(400).send({
-				error: "로그인 정보를 확인하세요",
-			});
-		} finally {
-			async () => {
-				await prisma.$disconnect();
-			};
-		}
-	},
-	userInfo: async (req: Request, res: Response) => {
-		const { user } = res.locals;
-		try {
-			return res.status(200).send({ message: "hi", userId: user.username });
-		} catch (error) {
-			console.log(error);
-			res.status(400).send({
-				error: "로그인 정보를 확인하세요",
-			});
-		} finally {
-			async () => {
-				await prisma.$disconnect();
-			};
-		}
-	},
+  //회원가입
+  signup: async (req: Request, res: Response) => {
+    const { userId, email, password, nickname } = req.body;
+    try {
+      //비밀번호 암호화
+      const encryptPassword = bcrypt.hashSync(password, saltRounds);
+      const data = {
+        username: userId,
+        email: email,
+        nickname: nickname,
+        password: encryptPassword,
+      };
+      //DB에 유저정보 create
+      await prisma.user.create({
+        data: data,
+      });
+      //response
+      res.send({
+        data: "success",
+      });
+    } catch (error) {
+      console.log(error);
+    } finally {
+      async () => {
+        await prisma.$disconnect();
+      };
+    }
+  },
+  //로그인
+  login: async (req: Request, res: Response) => {
+    const { userId, password } = req.body;
+    try {
+      //DB에서 유저 정보 찾기
+      const user = await prisma.user.findUnique({
+        where: {
+          username: userId,
+        },
+      });
+      //비밀번호 복호화 매치
+      const match = bcrypt.compareSync(password, user?.password ?? "");
+      if (!match) {
+        return res.status(400).send({ error: "로그인 정보를 확인하세요" });
+      }
+      //토큰 발행
+      const token = jwt.sign({ userId: user?.username }, "sangseon", {
+        expiresIn: "24h",
+      });
+      //response
+      res.status(200).send({ message: "로그인 성공", token });
+    } catch (error) {
+      console.log(error);
+      res.status(400).send({
+        error: "로그인 정보를 확인하세요",
+      });
+    } finally {
+      async () => {
+        await prisma.$disconnect();
+      };
+    }
+  },
+  //아이디 중복검사
+  confirmId: async (req: Request, res: Response) => {
+    try {
+      const { userId } = req.body;
+      const user = await prisma.user.findUnique({
+        where: {
+          username: userId,
+        },
+      });
+      if (user) {
+        res.send({
+          data: "fail",
+        });
+      } else {
+        res.send({
+          data: "success",
+        });
+      }
+    } catch (error) {
+      console.log(error);
+      res.status(400).send({
+        error: "로그인 정보를 확인하세요",
+      });
+    } finally {
+      async () => {
+        await prisma.$disconnect();
+      };
+    }
+  },
+  //닉네임 중복검사
+  confirmNick: async (req: Request, res: Response) => {
+    try {
+      const { nickname } = req.body;
+      const user = await prisma.user.findUnique({
+        where: {
+          nickname: nickname,
+        },
+      });
+      if (user) {
+        res.send({
+          data: "fail",
+        });
+      } else {
+        res.send({
+          data: "success",
+        });
+      }
+    } catch (error) {
+      console.log(error);
+      res.status(400).send({
+        error: "로그인 정보를 확인하세요",
+      });
+    } finally {
+      async () => {
+        await prisma.$disconnect();
+      };
+    }
+  },
+
+  //유저 정보
+  userInfo: async (req: Request, res: Response) => {
+    const { user } = res.locals;
+    try {
+      return res.status(200).send({ message: "hi", userId: user.username });
+    } catch (error) {
+      console.log(error);
+      res.status(400).send({
+        error: "로그인 정보를 확인하세요",
+      });
+    } finally {
+      async () => {
+        await prisma.$disconnect();
+      };
+    }
+  },
 };
 export default userController;
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,11 +1,14 @@
 import express from "express";
 import userController from "@controller/userController";
 import { authMiddleware } from "@middleware/authMiddleware";
+import passport from "passport";
 
 const userRouter = express.Router();
 
 userRouter.post("/signup", userController.signup);
 userRouter.post("/login", userController.login);
+userRouter.post("/confirmId", userController.confirmId);
+userRouter.post("/confirmNick", userController.confirmNick);
 userRouter.get("/me", authMiddleware, userController.userInfo);
 
 export default userRouter;
