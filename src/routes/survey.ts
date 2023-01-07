5d2562cef8475824bf4fda2283ed4891a8d3438d
reality023 <reality023@naver.com>
2023-01-07 09:21:42 +0900

    FEAT : 설문 기본 기능 완성 (수정필요)

--- src/routes/survey.ts
+++ src/routes/survey.ts
@@ -1,10 +1,12 @@
 import express, { Request, Response } from "express";
 import { surveyController } from "@controller/surveyController";
+import { authMiddleware } from "~/utils/middleware/authMiddleware";
 
 const router = express.Router();
 
-router.post("/", surveyController.addSurvey);
-router.get("/:id", surveyController.getSurvey);
-router.post("/:id", surveyController.submitSurvey);
+router.post("/", authMiddleware, surveyController.addSurvey);
+router.get("/:id", authMiddleware, surveyController.getSurvey);
+router.post("/:id", authMiddleware, surveyController.submitSurvey);
+router.get("/answer/:id", authMiddleware, surveyController.getSurveyAnswerList);
 
 export default router;
